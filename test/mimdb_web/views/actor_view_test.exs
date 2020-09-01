d345f46b37e480dbcecdf50fb7dc141e83131907
Laura Cordoba <castelinolaura@gmail.com>
2020-09-01 13:51:33 -0300

    Add testing for age method in Actors view

---
+++ test/mimdb_web/views/actor_view_test.exs
@@ -0,0 +1,15 @@
+defmodule MimdbWeb.ActorViewTest do
+  use MimdbWeb.ConnCase, async: true
+
+  import Phoenix.View
+
+  alias Mimdb.Actors
+  alias MimdbWeb.ActorView
+
+  @create_attrs %{birthdate: ~D[1956-07-09], name: "Tom Hanks"}
+
+  test "show correct age" do
+    {:ok, actor} = Actors.create_actor(@create_attrs)
+    assert ActorView.age(actor) == 64
+  end
+end
