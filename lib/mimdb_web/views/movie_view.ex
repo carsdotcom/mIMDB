83b0f6d190e967d110b533411e57c8be94d5d02f
Mike Ratliff <mratliff@gmail.com>
2020-09-11 12:51:38 -0600

    Adds most of Movie Edit Styling.

--- lib/mimdb_web/views/movie_view.ex
+++ lib/mimdb_web/views/movie_view.ex
@@ -16,4 +16,31 @@ defmodule MimdbWeb.MovieView do
     |> Enum.map(&{&1.name, &1.id})
     |> List.insert_at(0, {"All", 0})
   end
+
+  def builder() do
+    fn select_for ->
+      ~E"""
+      <div class="flex">
+        <div class="relative mr-2">
+          <%= select_for.(:month, [class: "mt-1 block appearance-none w-full border border-gray-300 text-gray-700 py-2 px-4 pr-8 rounded-md leading-tight focus:outline-none focus:bg-white focus:border-blue-300"]) %>
+          <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+          </div>
+        </div>
+        <div class="relative mr-2">
+          <%= select_for.(:day, [class: "mt-1 block appearance-none w-full border border-gray-300 text-gray-700 py-2 px-4 pr-8 rounded-md leading-tight focus:outline-none focus:bg-white focus:border-blue-300"]) %>
+          <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+          </div>
+        </div>
+        <div class="relative">
+          <%= select_for.(:year, [class: "mt-1 block appearance-none w-full border border-gray-300 text-gray-700 py-2 px-4 pr-8 rounded-md leading-tight focus:outline-none focus:bg-white focus:border-blue-300"]) %>
+          <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+          </div>
+        </div>
+      </div>
+      """
+    end
+  end
 end
